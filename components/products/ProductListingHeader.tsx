145267df9a6474832b0419c0cadd374715258a9d
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-03-31 19:43:55 +0530

    refactor(products): update and fixes layout

--- components/products/ProductListingHeader.tsx
+++ components/products/ProductListingHeader.tsx
@@ -4,24 +4,25 @@ import styles from "./ProductListingHeader.module.css";
 export default function ProductListingHeader() {
   return (
     <div className={styles.wrapper}>
-      <span className={styles.itemCount}>3425 ITEMS</span>
+      <div className={styles.leftSide}>
+        <span className={styles.itemCount}>3425 ITEMS</span>
+        <span className={styles.separator}>&gt;</span>
+        <span className={styles.filterDesktop}>SHOW FILTER</span>
+      </div>
 
-      <div className={styles.actions}>
-        <button className={styles.btn} type="button" aria-label="Show filters">
-          <span className={styles.filterMobile}>FILTER</span>
-          <span className={styles.filterDesktop}>SHOW FILTER</span>
-        </button>
+      <span className={styles.filterMobile}>FILTER</span>
 
-        <button className={styles.btn} type="button" aria-label="Sort products">
-          <span>RECOMMENDED</span>
-          <Image
-            className={styles.arrowIcon}
-            src="/arrow-down.svg"
-            alt=""
-            width={16}
-            height={16}
-          />
-        </button>
+      <div className={styles.divider}>|</div>
+
+      <div className={styles.rightSide}>
+        <span className={styles.recommendedText}>RECOMMENDED</span>
+        <Image
+          className={styles.arrowIcon}
+          src="/arrow-down.svg"
+          alt=""
+          width={16}
+          height={16}
+        />
       </div>
     </div>
   );
