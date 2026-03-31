4d851d86af94941b1c2d59ded593fe2abf92455d
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-03-31 13:20:19 +0530

    refactor(header) make the header responsive including logo section and nav-links section

--- components/header/LogoSection.tsx
+++ components/header/LogoSection.tsx
@@ -5,7 +5,20 @@ export default function LogoSection() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.left}>
-        <Image src="/Logo.svg" alt="logo" width={36} height={36} />
+        <Image
+          className={`${styles.icon} ${styles.hideTablet}`}
+          src="/menu-icon.svg"
+          alt="Open menu"
+          width={20}
+          height={20}
+        />
+        <Image
+          className={styles.icon}
+          src="/Logo.svg"
+          alt="Logo"
+          width={36}
+          height={36}
+        />
       </div>
 
       <div className={styles.center}>
@@ -13,12 +26,36 @@ export default function LogoSection() {
       </div>
 
       <div className={styles.right}>
-        <Image src="/search-normal.svg" alt="" width={24} height={24} />
-        <Image src="/heart.svg" alt="" width={24} height={24} />
-        <Image src="/shopping-bag.svg" alt="" width={24} height={24} />
-        <Image src="/profile.svg" alt="" width={24} height={24} />
+        <Image
+          className={styles.icon}
+          src="/search-normal.svg"
+          alt="Search"
+          width={24}
+          height={24}
+        />
+        <Image
+          className={styles.icon}
+          src="/heart.svg"
+          alt="Favorites"
+          width={24}
+          height={24}
+        />
+        <Image
+          className={styles.icon}
+          src="/shopping-bag.svg"
+          alt="Cart"
+          width={24}
+          height={24}
+        />
 
-        <div className={styles.lang}>ENG</div>
+        <Image
+          className={`${styles.icon} ${styles.hideMobile}`}
+          src="/profile.svg"
+          alt="Account"
+          width={24}
+          height={24}
+        />
+        <div className={`${styles.lang} ${styles.hideMobileBlock}`}>ENG</div>
       </div>
     </div>
   );
