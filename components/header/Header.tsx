cb1a3b5394a49eb9855ba61acb14e49e4be545db
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-03-31 11:23:20 +0530

    feat(header): add bottom border

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,14 @@
 import AnnouncementBar from "./AnnouncementBar";
 import HeaderNav from "./HeaderNav";
 import LogoSection from "./LogoSection";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <>
+    <header className={styles.header}>
       <AnnouncementBar />
       <LogoSection />
       <HeaderNav />
-    </>
+    </header>
   );
 }
